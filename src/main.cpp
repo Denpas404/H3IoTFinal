0813413a503fd91e2fc2a044edfbb13b6f0b56fe
Denpas404 <102860689+Denpas404@users.noreply.github.com>
2024-05-31 17:03:09 +0200

    Add Libs and data folder

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include <Arduino.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
 #include <Fs.h>
 #include <LittleFS.h>
 #include "sd_read_write.h"
@@ -8,24 +10,53 @@
 #define SD_MMC_CLK 39 // Please do not modify it.
 #define SD_MMC_D0 40  // Please do not modify it.
 
+// GPIO where the DS18B20 is connected to
+const int oneWireBus = 4;
+
+// Setup a oneWire instance to communicate with any OneWire devices
+OneWire oneWire(oneWireBus);
+
+// Pass our oneWire reference to Dallas Temperature sensor
+DallasTemperature sensors(&oneWire);
+
+// Define functions
+void Temperatur();
+void InitSDCard();
+
 void setup()
 {
+  // Start the Serial Monitor
   Serial.begin(115200);
+
+  // Start the DS18B20 sensor
+  sensors.begin();
+
+  // Initialize the SD Card
+  InitSDCard();
+}
+
+void loop()
+{
+  Temperatur();
+
+  vTaskDelay(5000);
+}
+
+void Temperatur()
+{
+  sensors.requestTemperatures();
+  float temperatureC = sensors.getTempCByIndex(0);
+  Serial.print(temperatureC);
+  Serial.println("ºC");
+}
+
+void InitSdCard()
+{
+  // Start the SD Card
   SD_MMC.setPins(SD_MMC_CLK, SD_MMC_CMD, SD_MMC_D0);
   if (!SD_MMC.begin("/sdcard", true, true, SDMMC_FREQ_DEFAULT, 5))
   {
     Serial.println("Card Mount Failed");
     return;
   }
-
-
-  
-  
-
-}
-
-
-
-void loop()
-{
 }
